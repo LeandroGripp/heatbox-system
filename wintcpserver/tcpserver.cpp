e723b06484aff16667574ccbbe25bc331d7f0fd5
Gabriel P. G <gabpgomes@gmail.com>
2023-05-24 17:06:11 -0300

    Untested! tcpserver initial version

--- wintcpserver/tcpserver.cpp
+++ wintcpserver/tcpserver.cpp
@@ -39,9 +39,10 @@ int main()
 	SOCKADDR_IN   ServerAddr;
 
 	char msgreq[TAMMSGREQ + 1]; //Centro de operações solicita dados
-	char msgack[TAMMSGACK + 1]; //ACK
+	char msgack[TAMMSGACK + 1]; //ACK do centro de operações
+	char msgackclp[TAMMSGACK + 1] = "44|000000"; //ACK do clp
 	char msgpar[TAMMSGPAR + 1]; //Centro de operações envia parâmetros
-	char msgdados[TAMMSGDADOS + 1]; //Hotbox envia dados sobre a linha ferroviária
+	char msgdados[TAMMSGDADOS + 1] = "09|000000|015|PTN-034234|0115.3|02|27/04/2023 10:12:33";//Hotbox envia dados sobre a linha ferroviária
 
 	char buf[100];
 	char msgcode[3];
@@ -158,8 +159,9 @@ int main()
 					exit(0);
 				}
 
-				// tratamento da mensagem de comandos
+				// mensagem de parâmetros de sinalização
 				if (status == TAMMSGPAR) {
+					/* verifica código da msg*/
 					if (strncmp(buf, "00", 2) != 0) {
 						strncpy(msgcode, buf, 2);
 						msgcode[2] = 0x00;
@@ -172,17 +174,39 @@ int main()
 						SetConsoleTextAttribute(hOut, WHITE);
 						exit(0);
 					}
-
+					/* recebe a mensagem de comandos de sinalização*/
 					strncpy(msgpar, buf, TAMMSGPAR);
 					msgpar[TAMMSGPAR] = 0x00;
 					SetConsoleTextAttribute(hOut, YELLOW);
 					printf("Mensagem de comandos recebida do Centro de Operações:\n%s\n\n",
 						msgpar);
 
-					//agora devolve um ACK
+					/* Envia ACK ao Centro de Operações */
+					sprintf(buf, "%06d", ++nseql);
+					memcpy(&msgackclp[3], buf, 6);
+					status = send(new_s, msgackclp, TAMMSGACK, 0);
+					if (status == TAMMSGACK) {
+						msgackclp[TAMMSGACK] = 0x00;
+						SetConsoleTextAttribute(hOut, HLBLUE);
+						printf("Mensagem de ACK enviada ao Centro de Operações [%s]:\n%s\n\n",
+							ipaddr, msgackclp);
+					}
+					else {
+						SetConsoleTextAttribute(hOut, HLRED);
+						if (status == 0)
+							// Esta situação NÃO deve ocorrer, e indica algum tipo de erro.
+							printf("SEND (3) retornou 0 bytes ... Falha!\n");
+						else if (status == SOCKET_ERROR) {
+							status = WSAGetLastError();
+							printf("Falha no SEND (3): codigo de erro = %d\n", status);
+						}
+						printf("Tentando reconexão ..\n\n");
+						break;
+					}
 				}
-				//tratamento da mensagem de requisição de dados
-				else if (status == TAMMSGREQ){
+				//mensagem de requisição de dados
+				else if (status == TAMMSGREQ) {
+					/* verifica código da msg*/
 					if (strncmp(buf, "99", 2) != 0) {
 						strncpy(msgcode, buf, 2);
 						msgcode[2] = 0x00;
@@ -196,11 +220,86 @@ int main()
 						exit(0);
 					}
 
-					//agora devolve os dados e espera um ACK
-				}
+					/* recebe a mensagem de comandos de sinalização*/
+					strncpy(msgreq, buf, TAMMSGREQ);
+					msgreq[TAMMSGREQ] = 0x00;
+					SetConsoleTextAttribute(hOut, YELLOW);
+					printf("Mensagem de requisição de dados recebida do Centro de Operações:\n%s\n\n",
+						msgreq);
 
+					/* envio de dados para o Centro de Operações*/
+					sprintf(buf, "%06d", ++nseql);
+					memcpy(&msgdados[3], buf, 6);
+					status = send(new_s, msgdados, TAMMSGDADOS, 0);
+					if (status == TAMMSGDADOS) {
+						msgack[TAMMSGDADOS] = 0x00;
+						SetConsoleTextAttribute(hOut, MAGENTA);
+						printf("Mensagem de dados enviada ao Centro de operações:\n%s\n\n",
+							msgdados);
+					}
+					else {
+						SetConsoleTextAttribute(hOut, HLRED);
+						if (status == 0)
+							// Esta situação NÃO deve ocorrer, e indica algum tipo de erro.
+							printf("SEND (3) retornou 0 bytes ... Falha!\n");
+						else if (status == SOCKET_ERROR) {
+							status = WSAGetLastError();
+							printf("Falha no SEND (3): codigo de erro = %d\n", status);
+						}
+						printf("Tentando reconexão ..\n\n");
+						break;
+					}
+
+					/* aguarda o ACK do Centro de Operações*/
+					memset(buf, sizeof(buf), 0);
+					status = recv(new_s, buf, TAMMSGACK, 0);
+					if (status == TAMMSGACK) {
+						if (strncmp(buf, "33", 2) != 0) {
+							strncpy(msgcode, buf, 2);
+							msgcode[2] = 0x00;
+							SetConsoleTextAttribute(hOut, HLRED);
+							printf("Codigo incorreto de mensagem de ACK: recebido '%s' ao inves de '33'\n", msgcode);
+							printf("Encerrando o programa...\n");
+							closesocket(s);
+							closesocket(new_s);
+							WSACleanup();
+							SetConsoleTextAttribute(hOut, WHITE);
+							exit(0);
+						}
+						sscanf(&buf[3], "%6d", &nseqr);
+						if (++nseql != nseqr) {
+							SetConsoleTextAttribute(hOut, HLRED);
+							printf("Numero sequencial de mensagem incorreto [1]: recebido %d ao inves de %d.\n",
+								nseqr, nseql);
+							printf("Encerrando o programa...\n");
+							closesocket(s);
+							closesocket(new_s);
+							WSACleanup();
+							SetConsoleTextAttribute(hOut, WHITE);
+							exit(0);
+						}
+						strncpy(msgack, buf, TAMMSGACK);
+						msgack[TAMMSGACK] = 0x00;
+						SetConsoleTextAttribute(hOut, MAGENTA);
+						printf("Mensagem de ACK recebida do CLP [%s]:\n%s\n\n", ipaddr, msgackclp);
+					}
+					else {
+						SetConsoleTextAttribute(hOut, HLRED);
+						if (status == 0)
+							// Esta situação NÃO deve ocorrer, e indica algum tipo de erro.
+							printf("RECV (4) retornou 0 bytes ... Falha!\n");
+						else if (status == SOCKET_ERROR) {
+							status = WSAGetLastError();
+							if (status == WSAETIMEDOUT) printf("Timeout no RECV (4)!\n");
+							else printf("Erro no RECV (4)! codigo de erro = %d\n", status);
+						}
+						printf("Tentando reconexao ..\n\n");
+						break;
+					}
+
+				}
 			}
-			//não recebeu o que esperava
+			//numero de bytes não corresponde a nenhuma mensagem
 			else {
 				SetConsoleTextAttribute(hOut, HLRED);
 				if (status == 0)
